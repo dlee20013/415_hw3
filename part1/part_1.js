391c5d1782a70fd559f6ff3d54120d59728ed300
dlee20013 <dlee20013@gmail.com>
2017-04-25 00:41:08 -0700

    ell

--- part1/part_1.js
+++ part1/part_1.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("signin-btn").onclick = function givePassword() {
-		alert(0);
+	document.getElementByClass("problem").innerHtml = "";
+	document.getElementByClass("error-message").innerHtml = "";
+	alert(0);
 	};
 });
 

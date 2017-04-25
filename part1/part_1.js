f5aa0b333ca6fa90751489a633a2c7c2c95ef127
dlee20013 <dlee20013@gmail.com>
2017-04-25 01:03:12 -0700

    hello

--- part1/part_1.js
+++ part1/part_1.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("signin-btn").onclick = function givePassword() {
 		alert(0);
 	};
-	document.getElementsByClass("problem").style.visibility = "hidden";
-	document.getElementsByClass("error-message").style.visibility = "hidden";
+	document.getElementsByClass("problem")[0].style.visibility = "hidden";
+	document.getElementsByClass("error-message")[0].style.visibility = "hidden";
 });
 

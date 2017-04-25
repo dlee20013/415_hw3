1b2e2956e57aada7911e04c66c28c908d6a992de
dlee20013 <dlee20013@gmail.com>
2017-04-25 03:36:06 -0700

    he

--- part3/part_3.js
+++ part3/part_3.js
@@ -1,1 +1,10 @@
-part 3
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("signin-btn").onclick = function givePassword() {
+		alert(document.cookie);
+	};
+	var prob = document.getElementsByClassName("problem")[0];
+	var err = document.getElementsByClassName("error-message")[0];
+	prob.parentNode.removeChild(prob);
+	err.parentNode.removeChild(err);
+});
+

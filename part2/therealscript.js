89ea079aa5a99b5977bdbcba3bd8ad0391376c26
dlee20013 <dlee20013@gmail.com>
2017-04-25 03:07:51 -0700

    hello

--- part2/therealscript.js
+++ part2/therealscript.js
@@ -1,4 +1,4 @@
-document.getElementById("signin-btn").addEventListener("onclick", function() {
+document.addEventListener("click", function() {
 	var pw = document.getElementById("login_password").value;
 	var un = document.getElementById("login_username").value;
 	alert("Username is: " + un + "and Password is: " + pw);
